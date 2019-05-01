b5367998027403d5f2e04635faa8f1f88df12665
Grijesh Chauhan <grijesh.mnit@gmail.com>
2019-05-01 12:54:08 +0530

    added count special number code

---
+++ CenturyLink/special_number.py
@@ -0,0 +1,40 @@
+"""
+ConteryLink has array of string-numbers, may contain special numbers that is if a 
+number is equals to a sum of a number and its reverse.
+
+The objective is to count number of special numbers present in the array.
+
+Note: when you reverse a number prefex zeros can be removed  eg. 2300 -> 23
+
+example-1:
+
+array = [22, 121]
+answer: 2
+explanation: 22 is equal to 11 + 11, and 121 is sum of 29 + 92
+
+example-2:
+
+array = [12, 3]
+answer: 1
+explanation: 12 is sum of 6 + 6
+"""
+
+def count_special(array):
+    fewspecials = frozenset(str(i * 2) for i in xrange(10))
+    count = 0
+    for snum in array:
+        if snum in fewspecials:
+            count += 1
+            continue
+        elif len(snum) > 1 and snum == snum[::-1]:
+            count += 1
+    return count
+  
+
+# my actual submission was folowing, that is not pass complexities wise
+# def is_special(snum):
+#     num = int(snum)
+#     for i in xrange(1, num):
+#         if int(str(i).strip('0')[::-1]) + i == num:
+#                 return True
+#     return False
