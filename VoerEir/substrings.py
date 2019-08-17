b6b602f586fd3c0512746d86ed8d64837d28de8b
Grijesh Chauhan <grijesh.mnit@gmaill.com>
2019-08-17 11:24:27 +0530

    VoerEir: renamed visited[] -> seens[], added helpful note

--- VoerEir/substrings.py
+++ VoerEir/substrings.py
@@ -1,20 +1,21 @@
 def lds(string: str) -> str:
     """ returns first longest distinct substring in input `string` """
-    visited = {}
+    seens = {}
     start, end, curt_start = 0, 0, 0
     for curt_end, c in enumerate(string):
         try:
-            last_seen = visited[c]
+            last_seen = seens[c]
             if last_seen < curt_start:
-                raise KeyError(f"'{c}' not found in {string[curt_start: curt_end]}")
+                raise KeyError(f"'{c}' not found in '{string[curt_start: curt_end]}'")
             if end - start <  curt_end - curt_start:
                 start, end = curt_start, curt_end
             curt_start = last_seen + 1
         except KeyError:
             pass
-        visited[c] = curt_end
+        seens[c] = curt_end
     else: 
-        # if longest substring is a suffix of the string!
+        # case when the longest substring is suffix of the string, here curt_end
+        # do not point to a repeating char hance included in the substring
         if string and end - start <  curt_end - curt_start + 1:
             start, end = curt_start, curt_end + 1
     return string[start: end]
