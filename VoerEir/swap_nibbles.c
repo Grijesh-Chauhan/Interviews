a47e45d105a911044d88a88144be0a7b41c1fcf2
Grijesh Chauhan <grijesh.mnit@gmaill.com>
2019-08-17 14:00:40 +0530

    VoerEir: adding C questions as swap_nibbles.c

---
+++ VoerEir/swap_nibbles.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdint.h>
+
+uint8_t swap_nibbles(uint8_t num) {
+    return (num & 0xF0) >> 4 | (num & 0x0F) << 4;
+}
+
+int main(int argc, char* argv[]) {
+    uint8_t num = 0xBC;  // 0b1011 1100
+    printf("swap_nibbles(%x) -> %x", num, swap_nibbles(num));
+    printf("\n");    
+    return EXIT_SUCCESS;
+}
+
+// other questions:
+// what is `uint8_t`?
+// what is size of char and 'A'
+
+
